e25f5fde8c938b6c40e244f99dc451c150c9642f
luckov2004 <luckov2004@mail.ru>
2017-08-14 05:11:18 +0000

    success

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def appication(environ, start_response):
-	body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+def hello(environ, start_response):
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 	start_response ('200 OK', [('Content-Type', 'text/plain')])
 	return body
--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py 
-#sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello hello:app &
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py 
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 -w 4 hello:hello &
+
+
 
