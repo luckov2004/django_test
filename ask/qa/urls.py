fdf995c55c38437ab48d3f4a15a6d4436976c4a0
luckov2004 <luckov2004@mail.ru>
2017-08-15 11:47:02 +0000

    django_dontStart

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+#from django.conf.urls import url
+#from django.contrib import admin
+from qa.views import test
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^$', test),
+	url(r'^login/', test),
+	url(r'^signup/', test),
+	url(r'^ask/', test),
+	url(r'^popular/', test),
+	url(r'^new/', test),
+	url(r'^question/(?P<pk>[0-9]+)', test),
+
 ]
--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/hello.py /usr/local/lib/python2.7/hello.py 
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 -w 4 hello:hello &
+sudo gunicorn  -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application &
+
 
 
 
