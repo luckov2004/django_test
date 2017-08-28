297f08862889c90d1451c4d2f70b173708972423
luckov2004 <luckov2004@mail.ru>
2017-08-28 05:38:07 +0000

    db_dw

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,24 +4,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Question(models.Model):
-	objects = QuestionManager()
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateField()
-	rating =models. int(10)
-	author = models.ForeignKey(User, on_delete=models.SET_NULL)
-	likes = models.ManyToManyField(User)
-
-class Answer(models.Model):
-	text = models.TextField()
-	added_at = models.DateField()
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.SET_NULL)
-
 class QuestionManager(models.Manager):
 	def new():
 		pass
 	def popular():
 		pass
+
+class Question(models.Model):
+	objects = QuestionManager()
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User, related_name = 'author_set')
+	likes = models.ManyToManyField(User, related_name = 'likes_set')
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.ForeignKey(Question) #, related_set = 'questions_set')
+	author = models.ForeignKey(User)#, related_name = 'author_set')
+
 
